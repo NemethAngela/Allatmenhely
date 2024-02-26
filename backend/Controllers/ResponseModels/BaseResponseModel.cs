f8e014c838deb2607651c2cba31376ed37232bc8
Nemeth Angela <nemethangela75@gmail.com>
2024-02-26 18:41:03 +0100

    jwt + login

---
+++ backend/Controllers/ResponseModels/BaseResponseModel.cs
@@ -0,0 +1,9 @@
+﻿namespace backend.Controllers.ResponseModels
+{
+    public class BaseResponseModel
+    {
+        public bool IsError { get; set; }
+        public string ErrorMessage { get; set; }
+        public Exception Exception { get; set; }
+    }
+}
---
+++ backend/Helpers/JWTHelper.cs
@@ -0,0 +1,30 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using backend.Models;
+using Microsoft.IdentityModel.Tokens;
+
+namespace backend.Helpers
+{
+    public class JWTHelper
+    {
+        private const string Secret = "TG9yZW0gSXBzdW0gaXMgc2ltcGx5IGR1bW15IHRleHQgb2YgdGhlIHByaW50aW5nIGFuZCB0eXBl\r\nc2V0dGluZyBpbmR1c3RyeS4gTG9yZW0gSXBzdW0gaGFzIGJlZW4gdGhlIGluZHVzdHJ5J3Mgc3Rh\r\nbmRhcmQgZHVtbXkgdGV4dCBldmVyIHNpbmNlIHRoZSAxNTAwcywgd2hlbiBhbiB1bmtub3duIHBy\r\naW50ZXIgdG9vayBhIGdhbGxleSBvZiB0eXBlIGFuZCBzY3JhbWJsZWQgaXQgdG8gbWFrZSBhIHR5\r\ncGUgc3BlY2ltZW4gYm9vay4gSXQgaGFzIHN1cnZpdmVkIG5vdCBvbmx5IGZpdmUgY2VudHVyaWVz\r\nLCBidXQgYWxzbyB0aGUgbGVhcCBpbnRvIGVsZWN0cm9uaWMgdHlwZXNldHRpbmcsIHJlbWFpbmlu\r\nZyBlc3NlbnRpYWxseSB1bmNoYW5nZWQuIEl0IHdhcyBwb3B1bGFyaXNlZCBpbiB0aGUgMTk2MHMg\r\nd2l0aCB0aGUgcmVsZWFzZSBvZiBMZXRyYXNldCBzaGVldHMgY29udGFpbmluZyBMb3JlbSBJcHN1\r\nbSBwYXNzYWdlcywgYW5kIG1vcmUgcmVjZW50bHkgd2l0aCBkZXNrdG9wIHB1Ymxpc2hpbmcgc29m\r\ndHdhcmUgbGlrZSBBbGR1cyBQYWdlTWFrZXIgaW5jbHVkaW5nIHZlcnNpb25zIG9mIExvcmVtIElw\r\nc3VtLg==";
+        private const int ExpirationHours = 24;
+
+        public static string GenerateToken(Admin adminUser)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Convert.FromBase64String(Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Email, adminUser.Email),
+                }),
+                Expires = DateTime.UtcNow.AddHours(ExpirationHours),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
