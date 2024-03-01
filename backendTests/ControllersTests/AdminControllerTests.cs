1ebef6074ebee8702fac33f511a990ca03810a49
Nemeth Angela <nemethangela75@gmail.com>
2024-03-01 18:15:51 +0100

    unit teszt kezdés

---
+++ backendTests/ControllersTests/AdminControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using backend.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace backend.Controllers.Tests
+{
+    [TestClass()]
+    public class AdminControllerTests
+    {
+        [TestMethod()]
+        public void LoginTest_ShouldWork()
+        {
+            //AdminController controller = new AdminController();
+
+        }
+
+        [TestMethod()]
+        public void LoginTest_ShouldFail()
+        {
+            //AdminController controller = new AdminController();
+
+        }
+    }
+}
---
+++ backendTests/ControllersTests/KindControllerTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace backendTests.ControllersTests
+{
+    [TestClass()]
+    public class KindControllerTests
+    {
+        [TestMethod()]
+        public void GetAllKindsTest()
+        {
+        }
+
+        [TestMethod()]
+        public void GetKindByIdTest()
+        {
+        }
+
+        [TestMethod()]
+        public void CreateKindTest()
+        {
+        }
+
+        [TestMethod()]
+        public void UpdateKindTest()
+        {
+        }
+
+        [TestMethod()]
+        public void DeleteKindTest()
+        {
+        }
+    }
+}
