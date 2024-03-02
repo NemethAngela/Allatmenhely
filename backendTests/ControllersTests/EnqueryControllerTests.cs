2a4dccb2ddce0fa887d2f46f7d4f449757d6eb43
Nemeth Angela <nemethangela75@gmail.com>
2024-03-02 13:05:16 +0100

    unit tesztek

---
+++ backendTests/ControllersTests/EnqueryControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace backend.Tests
+{
+    [TestClass()]
+    public class EnqueryControllerTests
+    {
+        [TestMethod()]
+        public void EnqueryControllerTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetAllEnqueriesTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetEnqueriesByAnimalIdTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
