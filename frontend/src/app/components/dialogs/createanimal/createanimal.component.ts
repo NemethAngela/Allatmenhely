b17933511bd9e2e28ace22693e4359b570d16c4d
Nemeth Angela <nemethangela75@gmail.com>
2024-03-24 14:47:30 +0100

    create animal

--- frontend/src/app/components/dialogs/createanimal/createanimal.component.ts
+++ frontend/src/app/components/dialogs/createanimal/createanimal.component.ts
@@ -13,6 +13,7 @@ import { Kind } from 'src/app/models/kind.model';
 })
 export class CreateanimalComponent {
     kinds: Kind[] = [];
+    selectedFile: File | null = null;
 
     name = new FormControl('test', [Validators.required]);
     kindId = new FormControl(0, [Validators.required]);
@@ -32,25 +33,48 @@ export class CreateanimalComponent {
 
     onSaveAnimalClick() {
         if (this.name.valid && this.kindId.valid && this.age.valid) {
-            const newAnimal: Animal = {
-                name: this.name.value !== null ? this.name.value : "",
-                kindId: this.kindId.value !== null && this.kindId.value !== undefined ? this.kindId.value : 0,
-                age: this.age.value !== null && this.age.value !== undefined ? this.age.value : 0,
-                isMale: this.isMale.value !== null ? this.isMale.value : false,
-                isNeutered: this.isNeutered.value !== null ? this.isNeutered.value : false,
-                description: this.description !== null ? this.description.value : "",
-            }
+            this.convertImageToBase64().then(base64String => {
+                const newAnimal: Animal = {
+                    name: this.name.value !== null ? this.name.value : "",
+                    kindId: this.kindId.value !== null && this.kindId.value !== undefined ? this.kindId.value : 0,
+                    age: this.age.value !== null && this.age.value !== undefined ? this.age.value : 0,
+                    isMale: this.isMale.value !== null ? this.isMale.value : false,
+                    isNeutered: this.isNeutered.value !== null ? this.isNeutered.value : false,
+                    description: this.description !== null ? this.description.value : "",
+                    photo: base64String
+                };
 
-            this.animalService.createAnimal(newAnimal).subscribe(
-                result => {
-                    console.log('Állatinfó: ', newAnimal);
-                    this.dialogRef.close();
-                },
-                error => {
-                    console.log('Mentési hiba!', error);
-                    console.log('Állatinfó: ', newAnimal);
-                }
-            )
+                // Elküldjük az új állatot a szolgáltatásnak
+                this.animalService.createAnimal(newAnimal).subscribe(
+                    result => {
+                        console.log('Állatinfó: ', newAnimal);
+                        this.dialogRef.close();
+                    },
+                    error => {
+                        console.log('Mentési hiba!', error);
+                        console.log('Állatinfó: ', newAnimal);
+                    }
+                );
+            });
         }
+    }
+
+    onFileSelected(event: any) {
+        this.selectedFile = event.target.files[0];
+    }
+
+    private convertImageToBase64(): Promise<string> {
+        return new Promise((resolve, reject) => {
+            if (this.selectedFile) {
+                const reader = new FileReader();
+                reader.readAsDataURL(this.selectedFile);
+                reader.onload = () => {
+                    resolve(reader.result as string);
+                };
+                reader.onerror = error => reject(error);
+            } else {
+                resolve('');
+            }
+        });
     }
 }
