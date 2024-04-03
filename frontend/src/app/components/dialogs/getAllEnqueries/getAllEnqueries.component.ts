5fd4b17e5c17670616e8c3593d319ce863fb65b7
KiralyPeter <peter.kiral@gmail.com>
2024-04-03 17:28:27 +0200

    Jelentkezések listája átalakítva; Dátum lokalizáció, Lapozás feliratai magyarul jelennek meg.

--- frontend/src/app/components/dialogs/getAllEnqueries/getAllEnqueries.component.ts
+++ frontend/src/app/components/dialogs/getAllEnqueries/getAllEnqueries.component.ts
@@ -1,17 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, Inject } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogRef, MatDialogConfig, MatDialog } from '@angular/material/dialog';
 import { Animal } from 'src/app/models/animal.model';
 import { Enquery } from 'src/app/models/enquery.model';
 import { EnqueryService } from 'src/app/services/enquery.service';
 import { EnqueriesResponseModel } from 'src/app/models/enqueriesresponsemodel.model'
-import { AfterViewInit, ViewChild } from '@angular/core';
-import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { AfterViewInit, ViewChild, LOCALE_ID } from '@angular/core';
+import { MatPaginator, MatPaginatorModule,MatPaginatorIntl } from '@angular/material/paginator';
 import { MatSort, MatSortModule } from '@angular/material/sort';
 import { MatTableDataSource, MatTableDataSourcePaginator, MatTableModule } from '@angular/material/table';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { DatePipe } from '@angular/common';
 
 @Component({
     selector: 'app-get-all-enqueries',
@@ -22,10 +23,12 @@ export class GetAllEnqueriesComponent implements AfterViewInit {
 
     // enqueries: Enquery[] = [];
 
-    displayedColumns: string[] = ['photo', 'name', 'timeStamp', 'phone', 'animalId', 'email'];
+    displayedColumns: string[] = ['photo', 'name', 'phone', 'email', 'timeStamp'];
     listOfEnqueries: Enquery[] = [];
     dataSource: any;
     dataLoaded = false;
+
+    preferredLocale: string;
 
     @ViewChild(MatPaginator) paginator!: MatPaginator;
     @ViewChild(MatSort) sort!: MatSort;
@@ -33,7 +36,16 @@ export class GetAllEnqueriesComponent implements AfterViewInit {
     constructor(
         private enqueryService: EnqueryService,
         private dialogRef: MatDialogRef<GetAllEnqueriesComponent>,
-    ) { }
+        @Inject(LOCALE_ID) private locale: string,
+        @Inject(MatPaginatorIntl) private paginatorIntl: MatPaginatorIntl
+    ) { 
+        this.preferredLocale = this.locale;     
+        this.paginatorIntl.itemsPerPageLabel = 'Találatok száma oldalanként:'; 
+        this.paginatorIntl.nextPageLabel = 'Következő oldal'; 
+        this.paginatorIntl.previousPageLabel = 'Előző oldal'; 
+        this.paginatorIntl.firstPageLabel = 'Első oldal';
+        this.paginatorIntl.lastPageLabel = 'Utolsó oldal';
+    }
 
     ngAfterViewInit() {
         this.enqueryService.getAllEnqueries().subscribe(response => {
@@ -46,8 +58,7 @@ export class GetAllEnqueriesComponent implements AfterViewInit {
     }
 
     applyFilter(event: Event) {
-        if(this.dataSource !== undefined)
-        {
+        if (this.dataSource !== undefined) {
             const filterValue = (event.target as HTMLInputElement).value;
             this.dataSource.filter = filterValue.trim().toLowerCase();
 
@@ -57,7 +68,7 @@ export class GetAllEnqueriesComponent implements AfterViewInit {
         }
     }
 
-    onCancelClick($event: MouseEvent){
+    onCancelClick($event: MouseEvent) {
         this.dialogRef.close();
     }
 
